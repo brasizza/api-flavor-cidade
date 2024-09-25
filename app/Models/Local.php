a3fb2c8c9980164ef96b02951c4e5c3b5fcb6408
Marcus Brasizza <marcus.brasizza@epoc.tech>
2024-09-25 13:52:33 -0300

    mutator

--- app/Models/Local.php
+++ app/Models/Local.php
@@ -9,4 +9,14 @@ class Local extends Model
 {
     protected $table = 'locais';
     use HasFactory;
+
+
+    public function getImagemAttribute($value)
+    {
+        // Get the base URL from the environment file
+        $baseUrl = env('URL_IMAGE');
+
+        // Return the full URL by appending the image path from the database
+        return $baseUrl . $value;
+    }
 }
